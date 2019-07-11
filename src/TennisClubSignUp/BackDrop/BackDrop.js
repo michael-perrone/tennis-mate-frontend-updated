38ce6681d944688b6e8b485442688ac02786f9aa
Michael Perrone <mikesdatascience@yahoo.com>
2019-07-11 17:56:36 -0400

    going pretty solid

--- src/TennisClubSignUp/BackDrop/BackDrop.js
+++ src/TennisClubSignUp/BackDrop/BackDrop.js
@@ -3,7 +3,7 @@ import styles from "./Backdrop.module.css";
 
 class BackDrop extends React.Component {
   render() {
-    return <div onClick={this.props.unShowConfirmModal} id={styles.Backdrop} />;
+    return <div onClick={this.props.unShowConfirmModal} id={styles.backDrop} />;
   }
 }
 
