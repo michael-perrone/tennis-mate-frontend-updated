70836580de5ee8d0f6e3678adc66fbe2d100d019
Michael Perrone <mikesdatascience@yahoo.com>
2019-07-27 18:58:03 -0400

    good fix

--- src/TennisClubSignUp/TennisClubSignupLeftSide/TennisClubForm/GoBackToAdmin/GoBackToAdmin.js
+++ src/TennisClubSignUp/TennisClubSignupLeftSide/TennisClubForm/GoBackToAdmin/GoBackToAdmin.js
@@ -4,7 +4,13 @@ import styles from "./GoBackToAdmin.module.css";
 class GoBackToAdmin extends React.Component {
   render() {
     return (
-      <button id={styles.goBack} onClick={this.props.unEnterAdmin}>
+      <button
+        id={styles.goBack}
+        onClick={e => {
+          e.preventDefault();
+          this.props.unEnterAdmin();
+        }}
+      >
         Back
       </button>
     );
