4235729abfc42f94bff60a24507e7f9bad9d2e4f
Michael Perrone <mikesdatascience@yahoo.com>
2019-08-02 23:09:31 -0400

    form going

--- src/InstructorHome/InstructorProfileCreate/InstructorProfileCreateForm/InstructorProfileCreateForm.js
+++ src/InstructorHome/InstructorProfileCreate/InstructorProfileCreateForm/InstructorProfileCreateForm.js
@@ -6,20 +6,36 @@ class InstructorProfileCreateForm extends React.Component {
     super(props);
     this.state = {
       formSelectors: [
-        { name: "Job Experience", selected: false },
-        { name: "Years Teaching", selected: false },
-        { name: "Location", selected: false },
-        { name: "Certifications", selected: false },
-        { name: "Rankings", selected: false },
-        { name: "Player Specialization", selected: false },
-        { name: "Lesson Rate", selected: false },
-        { name: "Photo", selected: false }
-      ]
+        { name: "Job Experience", selected: true, completed: false },
+        { name: "Years Teaching", selected: false, completed: false },
+        { name: "Location", selected: false, completed: false },
+        { name: "Certifications", selected: false, completed: false },
+        { name: "Rankings", selected: false, completed: false },
+        { name: "Player Specialization", selected: false, completed: false },
+        { name: "Lesson Rate", selected: false, completed: false },
+        { name: "Photo", selected: false, completed: false }
+      ],
+
+      jobExperience: {
+        clubName: "",
+        from: "",
+        to: "",
+        jobTitle: "",
+        current: ""
+      }
     };
     this.changeSelected = this.changeSelected.bind(this);
   }
 
-  changeSelected(index) {}
+  changeSelected = index => () => {
+    const newFormSelectors = [...this.state.formSelectors];
+    for (let i = 0; i < newFormSelectors.length; i++) {
+      newFormSelectors[i].selected = false;
+    }
+    this.setState({ newFormSelectors: newFormSelectors });
+    newFormSelectors[index].selected = true;
+    this.setState({ formSelectors: newFormSelectors });
+  };
 
   render() {
     return (
@@ -28,6 +44,7 @@ class InstructorProfileCreateForm extends React.Component {
           {this.state.formSelectors.map((element, index) => {
             return (
               <p
+                onClick={this.changeSelected(index)}
                 key={element.name}
                 id={
                   element.selected
@@ -41,7 +58,17 @@ class InstructorProfileCreateForm extends React.Component {
             );
           })}
         </div>
-        <form id={styles.instructorProfileCreateForm} />
+        <form
+          style={{ marginTop: "100px" }}
+          id={styles.instructorProfileCreateForm}
+        >
+          <div>
+            <input value={this.state.jobExperience.clubName} name="clubName" />
+            <input value={this.state.jobExperience.from} name="from" />
+            <input value={this.state.jobExperience.to} name="to" />
+            <input value={this.state.jobExperience.jobTitle} name="jobTitle" />
+          </div>
+        </form>
       </div>
     );
   }
